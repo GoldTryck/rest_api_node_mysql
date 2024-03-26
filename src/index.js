1ae37ea591cc890cd966632a87007faf98c3b3f7
hector <lariosh69@gmail.com>
2024-03-26 13:51:53 -0600

    [HECTOR] Solicitudes no encontradas

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import indexRoutes from './routes/index.routes.js'
 
 const app = express()
 
+
 app.use(express.json())
 
 app.use(indexRoutes)
 app.use('/api/', employyesRoutes)
+
+app.use((req, res) => { // Toda solicitud que no coincid con las rutas definidas antes responderan:
+    res.status(404).json({
+        message: 'End point not found'
+    })
+})
 
 app.listen(3000)
 console.log('Server running on port 3000')
