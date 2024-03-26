e93663b8ac4c8a98650e739e4fe71fd962bb173f
hector <lariosh69@gmail.com>
2024-03-26 00:03:26 -0600

    [HECTOR] Creando los controladores

---
+++ src/controllers/employees.controller.js
@@ -0,0 +1,7 @@
+export const getEmployees = (req, res) => res.send('Buscando empleados')
+
+export const createEmployees = (req, res) => res.send('creando empleados')
+
+export const updateEmployees = (req, res) => res.send('Actualizando empleados')
+
+export const deleteEmployees = (req, res) => res.send('Eliminando empleados')
--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express'
+import {
+    getEmployees,
+    createEmployees,
+    updateEmployees,
+    deleteEmployees
+} from '../controllers/employees.controller.js'
 
 const router = Router()
 
 //Aqui no existe app usamos router en su lugar
-router.get('/employees', (req, res) => res.send('Buscando empleados'))
-router.post('/employees', (req, res) => res.send('creando empleados'))
-router.put('/employees', (req, res) => res.send('Actualizando empleados'))
-router.delete('/employees', (req, res) => res.send('Eliminando empleados'))
+router.get('/employees', getEmployees)
+router.post('/employees', createEmployees)
+router.put('/employees', updateEmployees)
+router.delete('/employees', deleteEmployees)
 export default router
